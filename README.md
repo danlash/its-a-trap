86ca1c152072ed884868d7d8935fdf92c11f661c
Dan Lash <danlash@gmail.com>
2013-10-18 21:59:11 -0400

    first attempt at toggling a pin on raspberry pi

---
+++ README.md
@@ -0,0 +1,14 @@
+Halloween... IT'S A TRAP!
+===
+
+This is a raspberry pi project for a Halloween costume accessory. A large button glowing with an LED attached to the cosume, that when pressed triggers a clip of a random song played through a the audio port. The LED flashes different colors while playing.
+
+
+Details
+---
+
+- raspberry pi hardware
+- node.js application
+- button with led inside (dome touch light)
+- toggle switch from light
+- plays random mp3 file when button is pressed
---
+++ app.js
@@ -0,0 +1,34 @@
+var CHECK_EVERY_MILLISECONDS = 1000;
+
+var gpio = require('gpio');
+
+var buttonToggled = false;
+var gpio11;
+
+function trap() { 
+  if (buttonPressed()) {
+    if (gpio11.value === 0)
+      gpio11.set();
+    else
+      gpio11.set(0);
+  }
+}
+
+function buttonPressed() {
+  return true;
+}
+
+function loopEvery(loopFn, timeoutMilliseconds) {
+  var looper = function() { loopFn(); setTimeout(looper); };
+  looper();
+}
+
+function init() {
+  var gpio11 = gpio.export(11, { direction: 'out' });
+}
+
+
+init(function(){
+  //main loop
+  loopEvery(trap, timeoutMilliseconds);
+});
